3b367a573aab2189fbd9bbb9f7bcd5227d2ed4fa
Casey Carroll <caseydean8@gmail.com>
2022-07-18 17:31:39 -0400

    Recent Post widget fetches data from Hygraph

--- services/index.js
+++ services/index.js
@@ -4,13 +4,9 @@ import { request, gql, GraphQLClient } from "graphql-request";
 // Apparently GraphQLClient is modern way to get to endpoint...
 // const graphqlAPI = process.env.GRAPHCMS_ENDPOINT;
 
+const graphQLClient = new GraphQLClient(process.env.GRAPHCMS_ENDPOINT);
 
 export const getPosts = async () => {
-
-  const graphQLClient = new GraphQLClient(
-    process.env.GRAPHCMS_ENDPOINT 
-  );
-
   const query = gql`
     query MyQuery {
       postsConnection {
@@ -44,4 +40,50 @@ export const getPosts = async () => {
   const result = await graphQLClient.request(query);
 
   return result.postsConnection.edges;
+};
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query getPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await graphQLClient.request(query);
+
+  return result.posts;
+};
+
+export const getSimilarPosts = async () => {
+  const query = gql`
+    query getPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        # Since we're in related posts sidebar we don't want to retrieve current slug
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await graphQLClient.request(query);
+
+  return result.posts;
 };
