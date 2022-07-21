dcc7d126b0c54826fd67fce9adc1bef02c916516
Casey Carroll <caseydean8@gmail.com>
2022-07-21 11:44:48 -0400

    Comments rendered from hygraph

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import parse from "html-react-parser";
+// Curly braces are for named (often multiple) exports from a file. They are not needed for a default export
+import { getComments } from "../services";
+import { comment } from "postcss";
 
-const Comments = () => {
+const Comments = ({ slug }) => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    getComments(slug).then((result) => setComments(result));
+  }, []);
+
   return (
-    <div>
-      <h1>Comments</h1>
-    </div>
-  )
-}
+    <>
+      {comment.length && (
+        <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+          <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+            {comments.length} {comments.length > 1 ? "Comments" : "Comment"}
+          </h3>
+          {comments.map((comment) => (
+            <div className="border-b border-gray-100 mb-4 pb-4">
+              <p className="mb-4">
+                <span className="font-semibold">{comment.name}</span> on{" "}
+                {moment(comment.createdAt).format("MMM DD, YYYY")}
+              </p>
+              <p className="whitespace-pre-line text-gray-600 w-full">
+                {parse(comment.comment)}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
 
-export default Comments
+export default Comments;
