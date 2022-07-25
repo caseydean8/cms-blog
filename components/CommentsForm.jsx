414fb5b9c7ef2ef224d7800a5340e143d19f9e63
Casey Carroll <caseydean8@gmail.com>
2022-07-25 15:42:32 -0400

    Comment form clears after submit

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,32 +1,50 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "./Button";
 
 import { submitComment } from "../services";
 
 const CommentsForm = ({ slug }) => {
-  // useState snippet, type "error", press tab to capitalize
   const [error, setError] = useState(false);
   const [localStorage, setLocalStorage] = useState(null);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const commentEl = useRef();
-  const nameEl = useRef();
-  const emailEl = useRef();
-  const storeDataEl = useRef();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    comment: "",
+    storeData: false,
+  });
 
   useEffect(() => {
     setLocalStorage(window.localStorage);
-    nameEl.current.value = window.localStorage.getItem("name");
-    emailEl.current.value = window.localStorage.getItem("email");
+    const initialFormData = {
+      name: window.localStorage.getItem("name"),
+      email: window.localStorage.getItem("email"),
+      storeData:
+        window.localStorage.getItem("name") ||
+        window.localStorage.getItem("email"),
+    };
+    setFormData(initialFormData);
   }, []);
+
+  const onInputChange = (e) => {
+    const { target } = e;
+    if (target.type === "checkbox") {
+      setFormData((prevState) => ({
+        ...prevState,
+        [target.name]: target.checked,
+      }));
+    } else {
+      setFormData((prevState) => ({
+        ...prevState,
+        [target.name]: target.value,
+      }));
+    }
+  };
 
   const handleCommentSubmit = () => {
     setError(false);
 
-    const { value: comment } = commentEl.current;
-    const { value: name } = nameEl.current;
-    const { value: email } = emailEl.current;
-    const { checked: storeData } = storeDataEl.current;
-
+    const { name, email, comment, storeData } = formData;
     if (!comment || !name || !email) {
       setError(true);
       return;
@@ -44,6 +62,17 @@ const CommentsForm = ({ slug }) => {
     }
 
     submitComment(commentObj).then((res) => {
+      if (res.createComment) {
+        if (!storeData) {
+          formData.name = "";
+          formData.email = "";
+        }
+        formData.comment = "";
+      }
+      setFormData((prevState) => ({
+        ...prevState,
+        ...formData,
+      }));
       setShowSuccessMessage(true);
       setTimeout(() => {
         setShowSuccessMessage(false);
@@ -58,7 +87,8 @@ const CommentsForm = ({ slug }) => {
       </h3>
       <div className="grid grid-cols-1 gap-4 mb-4">
         <textarea
-          ref={commentEl}
+          onChange={onInputChange}
+          value={formData.comment}
           className="p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
           placeholder="Comment"
           name="comment"
@@ -67,14 +97,16 @@ const CommentsForm = ({ slug }) => {
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
         <input
           type="text"
-          ref={nameEl}
+          onChange={onInputChange}
+          value={formData.name}
           className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
           placeholder="Name"
           name="name"
         />
         <input
           type="email"
-          ref={emailEl}
+          onChange={onInputChange}
+          value={formData.email}
           className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
           placeholder="Email"
           name="email"
@@ -83,7 +115,8 @@ const CommentsForm = ({ slug }) => {
       <div className="grid grid-cols-1 gap-4 mb-4">
         <div>
           <input
-            ref={storeDataEl}
+            checked={formData.storeData}
+            onChange={onInputChange}
             type="checkbox"
             id="storeData"
             name="storeData"
