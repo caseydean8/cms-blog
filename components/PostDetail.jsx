336566e6c3ec530a54e08b03b953a4104b4433a5
Casey Carroll <caseydean8@gmail.com>
2022-07-26 16:25:00 -0400

    Date stays on same line

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -74,8 +74,10 @@ const PostDetail = ({ post }) => {
         />
       </div>
       <div className="px-4 lg:px-0">
+        {/* div holding name and date on post page*/}
         <div className="flex items-center mb-8 w-full">
-          <div className="flex items-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
+          {/* div holding image and name */}
+          <div className="flex items-center lg:mb-0 lg:w-auto mr-8">
             <img
               src={post.author.photo.url}
               alt={post.author.name}
@@ -87,10 +89,12 @@ const PostDetail = ({ post }) => {
               {post.author.name}
             </p>
           </div>
-          <div className="font-medium text-gray-700">
+          
+        {/* div holding svg and date */}
+          <div className="flex items-center font-medium text-gray-700">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 inline mr-2 text-pink-500"
+              className="h-6 w-6 inline mr-2 text-teal align-middle"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -102,7 +106,7 @@ const PostDetail = ({ post }) => {
                 d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
               />
             </svg>
-            <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
+            <p className="inline align-middle">{moment(post.createdAt).format("MMM DD, YYYY")}</p>
           </div>
         </div>
         <h1 className="mb-8 text 3xl font-semibold">{post.title}</h1>
