40103c165e90aae4c6b8a664f50b31d531b7b7da
Casey Carroll <caseydean8@gmail.com>
2022-08-06 14:18:23 -0400

    Added Splash Component to home page

---
+++ components/Splash.jsx
@@ -0,0 +1,25 @@
+// shortcut rafce
+// import React from "react";
+import Typed from "react-typed";
+
+const Splash = () => {
+  return (
+    <div className="container mx-auto md:px-10 px-8 mb-8">
+      <h1 className="text-5xl font-medium py-2">let's go</h1>
+        <Typed
+          className="text-teal font-bold text-6xl pl-7 py-2"
+          strings={["React", "Node", "Mongo", "MySQL", "Express", "jQuery"]}
+          typeSpeed={100}
+          backSpeed={60}
+          backDelay={2100}
+          showCursor={false}
+          loop
+        />
+      <h4 className="font-medium py-2 text-xl">
+        Welcome to the Portfolio of Casey Carroll, Full Stack Developer
+      </h4>
+    </div>
+  );
+};
+
+export default Splash;
---
+++ pages/home/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Splash from "../../components/Splash"
+const index = () => {
+  return (
+    <Splash />
+  )
+}
+
+export default index
