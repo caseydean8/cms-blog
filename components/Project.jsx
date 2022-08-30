a94b202fbc8da9afad80f675ccc5cbd7b43b8e15
Casey Carroll <caseydean8@gmail.com>
2022-08-30 17:03:51 -0400

    Added close functionality to modal

--- components/Project.jsx
+++ components/Project.jsx
@@ -1,21 +1,14 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import ProjectModal from "./ProjectModal";
-import Portal from "./Portal";
-// import "./Project.css";
-// import ProjectModal from "./ProjectModal";
 
 function Project(props) {
-  // console.log(`props in Project.jsx`)
-  // console.log(props)
-
   const [isOpen, setIsOpen] = useState(false);
 
   function toggleModal() {
-    console.log(`toggle Modal`);
     setIsOpen(!isOpen);
   }
-
+  
   return (
     <div
       id={props.id}
@@ -24,8 +17,6 @@ function Project(props) {
       <div
         type="button"
         onClick={toggleModal}
-        // data-bs-toggle="modal"
-        // data-bs-target={`#modal-${props.id}`}
       >
         <Image
           className=""
@@ -35,10 +26,7 @@ function Project(props) {
           height="338"
         />
       </div>
-      {/* <Portal open={isOpen} onClose={() => setIsOpen(false)} props={props}>
-        Portal stuff
-      </Portal> */}
-      <ProjectModal isOpen={isOpen} {...props} />
+      <ProjectModal isOpen={isOpen} handleClose={toggleModal} {...props} />
     </div>
   );
 }
--- components/ProjectModal.jsx
+++ components/ProjectModal.jsx
@@ -1,27 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import Modal from "react-modal";
 
 Modal.setAppElement("#__next");
 
 const ProjectModal = (props) => {
-  function toggleModal() {
-    console.log(`toggle Modal`);
-    setIsOpen(!isOpen);
-  }
 
   return (
     <Modal
       isOpen={props.isOpen}
       contentLabel="My dialog"
       className="mymodal"
-      closeTimeoutMS={500}
+      // closeTimeoutMS={1500}
     >
       <div
         className="fixed top-0 left-0 z-[1060] w-full h-full overflow-x-hidden overflow-y-auto outline-0 transition-opacity"
         id={`modal-${props.id}`}
       >
         <div className="transition-transform w-auto max-w-lg my-7 mx-auto relative">
-          <div className="relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding border border-solid border-stone-900 rounded outline-none">
+          <div className="relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding border border-solid border-stone-300 rounded outline-none">
             {/* may need border-top-radius: calc(.3rem - 1px) below */}
             <div className="flex shrink-0 items-center justify-between p-4 pb-0">
               <h1
@@ -32,23 +28,24 @@ const ProjectModal = (props) => {
               </h1>
               <button
                 type="button"
-                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
                 data-modal-toggle="defaultModal"
+                onClick={() => props.handleClose()}
               >
                 <svg
                   aria-hidden="true"
-                  class="w-5 h-5"
+                  className="w-5 h-5"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
-                <span class="sr-only">Close modal</span>
+                <span className="sr-only">Close modal</span>
               </button>
             </div>
             <div className="relative flex-auto p-4">
@@ -61,7 +58,6 @@ const ProjectModal = (props) => {
               <a
               // font-normal leading-normal text-center?
                 className="inline-block cursor-pointer select-none border border-teal text-teal rounded py-1.5 px-3 m-1"
-                target="_blank"
                 rel="noopener noreferrer"
                 href={props.link}
               >
@@ -69,7 +65,6 @@ const ProjectModal = (props) => {
               </a>
               <a
                 className="inline-block cursor-pointer select-none border border-teal text-teal rounded py-1.5 px-3 m-1"
-                target="_blank"
                 rel="noopener noreferrer"
                 href={props.repo}
               >
