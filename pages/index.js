6e9c122e3c7125f06554cbb1d55e8d9c09b0aa07
Casey Carroll <caseydean8@gmail.com>
2022-09-12 09:49:48 -0400

    Moved portfolio page to index

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,14 @@
-import Head from "next/head";
-import { PostCard, Categories, PostWidget } from "../components"; // index not needed
-import { getPosts } from "../services";
-import { FeaturedPosts } from "../sections"
+import React from "react";
+import Splash from "../components/Splash";
+import Gallery from "../components/Gallery";
 
-export default function Home({ posts }) {
+const index = () => {
   return (
-    <div className="container mx-auto md:px-10 px-8 mb-8">
-      <Head>
-        <title>Casey Carroll's Blog</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <FeaturedPosts />
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className="lg:col-span-8 col-span-1">
-          {posts.map((post) => (
-            <PostCard post={post.node} key={post.node.title} />
-          ))}
-        </div>
-        <div className="lg:col-span-4 col-span-1">
-          <div className="lg:sticky relative top-8">
-            <PostWidget />
-            <Categories />
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <Splash />
+      <Gallery />
+    </>
   );
 };
 
-export async function getStaticProps() {
-  const posts = (await getPosts()) || [];
-  return {
-    props: { posts },
-  };
-}
-
+export default index;
