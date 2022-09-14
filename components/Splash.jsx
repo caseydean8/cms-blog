d4b12f02ef93e9eb4ac3f28e5c2f46d2d4a8fefa
Casey Carroll <caseydean8@gmail.com>
2022-09-14 11:28:18 -0400

    Fixed spacing in splash component.

--- components/Splash.jsx
+++ components/Splash.jsx
@@ -6,8 +6,9 @@ const Splash = () => {
   return (
     <div className="my-8 max-w-5xl mx-auto">
       <h1 className="text-5xl font-semibold py-2">let's go</h1>
+      <div className="mb-4">
         <Typed
-          className="text-teal font-bold text-6xl pl-7 py-2"
+          className="text-teal font-bold text-6xl pl-7"
           strings={["React", "Node", "Mongo", "MySQL", "Express", "jQuery"]}
           typeSpeed={100}
           backSpeed={60}
@@ -15,6 +16,7 @@ const Splash = () => {
           showCursor={false}
           // loop
         />
+      </div>
       <h4 className="font-medium py-2 text-xl">
         Welcome to the Portfolio of Casey Carroll, Full Stack Developer
       </h4>
