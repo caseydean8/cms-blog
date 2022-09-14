99f78833ffe51481468a3dbfc2c6bd3400342207
Casey Carroll <caseydean8@gmail.com>
2022-09-14 12:25:22 -0400

    Imported @fontsource css for vercel

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { Layout } from "../components";
 import "tailwindcss/tailwind.css";
 import "../styles/globals.scss";
 import "@fontsource/poppins"
+import "@fontsource/poppins/700.css"
 
 function MyApp({ Component, pageProps }) {
   return (
